7935e841a6f48a00865588d7ec33f16eb1af76b1
17robots <mdray@ameritech.net>
2018-01-16 08:37:59 -0500

    Copied Over The Old 2017 Code And Added Blank Robot, OI and RobotMap Classes In Case We Wanted To Start Over

---
+++ 2018-Robot/src/org/usfirst/frc/team4121/robot/commands/NewAutoDriveCommand.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team4121.robot.commands;
+
+import org.usfirst.frc.team4121.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class NewAutoDriveCommand extends Command {
+
+	double leftMotor, rightMotor;
+	
+    public NewAutoDriveCommand(double left, double right) {
+        requires(Robot.driveTrain);
+        
+        leftMotor=left;
+        rightMotor=right;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.autoDrive(leftMotor, rightMotor);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
