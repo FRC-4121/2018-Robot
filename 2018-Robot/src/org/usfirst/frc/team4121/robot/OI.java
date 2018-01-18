effa7b60b41b032adb6569afec434c7621dd518c
17robots <mdray@ameritech.net>
2018-01-18 18:41:07 -0500

    I do not know anything anymore

--- 2018-Robot/src/org/usfirst/frc/team4121/robot/OI.java
+++ 2018-Robot/src/org/usfirst/frc/team4121/robot/OI.java
@@ -1,94 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
 package org.usfirst.frc.team4121.robot;
 
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
-
-import org.usfirst.frc.team4121.robot.commands.ClimbCommand;
-import org.usfirst.frc.team4121.robot.commands.CloseGateCommand;
-import org.usfirst.frc.team4121.robot.commands.DecreaseShootSpeedCommand;
-import org.usfirst.frc.team4121.robot.commands.FeedCommand;
-import org.usfirst.frc.team4121.robot.commands.FindBoilerTargetCommand;
-import org.usfirst.frc.team4121.robot.commands.FindGearTargetCommand;
-import org.usfirst.frc.team4121.robot.commands.IncreaseShootSpeedCommand;
-import org.usfirst.frc.team4121.robot.commands.OpenGateCommand;
-import org.usfirst.frc.team4121.robot.commands.ShiftDownCommand;
-import org.usfirst.frc.team4121.robot.commands.ShiftUpCommand;
-import org.usfirst.frc.team4121.robot.commands.ShootCommand;
-import org.usfirst.frc.team4121.robot.commands.ShootCommandGroup;
-import org.usfirst.frc.team4121.robot.commands.StopClimbCommand;
-import org.usfirst.frc.team4121.robot.commands.StopEverythingShootingCommandGroup;
-import org.usfirst.frc.team4121.robot.commands.StopShootCommand;
-import org.usfirst.frc.team4121.robot.commands.SwitchCommandGroup;
-import org.usfirst.frc.team4121.robot.commands.SwitchDriveCommand;
-
 /**
- * This is the main class that initializes and tells what buttons to do what.
- * 
- * @author Ben Hayden
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-	
-	//Initializations
-	public Joystick leftJoy, rightJoy;
-	public DigitalInput limitSwitch;
-	public ADXRS450_Gyro MainGyro;
-	public Encoder rightEncoder, leftEncoder;
-	public Button shoot, feed, climb, servo, shiftUp, shiftDown, gear, boiler, switchDrive, increaseShootSpeed, decreaseShootSpeed;
-	
-	public OI() {
-	
-		//Encoders
-		rightEncoder = new Encoder(0,1, false, Encoder.EncodingType.k4X);
-		rightEncoder.setDistancePerPulse(.05277);  //.03598, keep this rate it works
-		rightEncoder.setReverseDirection(true);
-		rightEncoder.setSamplesToAverage(7);
-		leftEncoder = new Encoder(2,3, false, Encoder.EncodingType.k4X);
-		leftEncoder.setDistancePerPulse(.05277);
-		leftEncoder.setSamplesToAverage(7);
-				
-		//Limit Switch
-		limitSwitch = new DigitalInput(4);      
-		
-		//Gyro
-		MainGyro = new ADXRS450_Gyro();
-				
-		//Joysticks
-		leftJoy = new Joystick(0);
-		rightJoy = new Joystick(1);
-	
-		//Buttons
-		shoot = new JoystickButton(rightJoy, 1);
-		//decreaseShootSpeed = new JoystickButton (rightJoy, 2);
-		//servo = new JoystickButton(rightJoy,2);
-		//increaseShootSpeed = new JoystickButton (rightJoy, 3);
-		switchDrive = new JoystickButton(rightJoy, 4);
-		//feed = new JoystickButton(rightJoy, 3);
-		climb = new JoystickButton(leftJoy, 1);
-		gear = new JoystickButton(leftJoy, 2);
-		boiler = new JoystickButton(leftJoy, 3);
-		shiftDown = new JoystickButton(leftJoy, 4);
-		shiftUp = new JoystickButton(leftJoy, 5);
-		
-		//Commands
-		shoot.whileHeld(new ShootCommand());//put logic in shoot command now
-		shoot.whenReleased(new StopEverythingShootingCommandGroup());
-		//servo.whileHeld(new OpenGateCommand());
-		//servo.whenReleased(new CloseGateCommand());
-		//feed.whileHeld(new FeedCommand());
-		climb.whileHeld(new ClimbCommand());
-		climb.whenReleased(new StopClimbCommand());
-		shiftUp.whenActive(new ShiftUpCommand());
-		shiftDown.whenActive(new ShiftDownCommand());
-		//gear.whenPressed(new FindGearTargetCommand());
-		//boiler.whenPressed(new FindBoilerTargetCommand());
-		switchDrive.whenPressed(new SwitchDriveCommand());
-		//decreaseShootSpeed.whenPressed(new DecreaseShootSpeedCommand());
-		//increaseShootSpeed.whenPressed(new IncreaseShootSpeedCommand());
-		
-	}
+	//// CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	//// joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
+
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
+
+	//// TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
+
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
+
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
+
+	// Start the command when the button is released and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
 }
